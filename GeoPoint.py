088f5f41b6234bc024fc49d13f6a536dd911d731
Adelita Martinez <amartinez1013@cnm.edu>
2024-07-18 09:01:01 -0600

    Added first comments

---
+++ GeoPoint.py
@@ -0,0 +1,5 @@
+# MartinezP11
+# Programmer: Adelita Martinez
+# Email: amartinez1013@cnm.edu
+# Purpose: Demonstrate how to define a class with a constructor and properties
+# Python Version: 3.12.3
